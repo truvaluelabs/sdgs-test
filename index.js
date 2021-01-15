b351f0e023a95cf45da9964de3b886a46f41b3f8
Cindy Ho <cindy.ho@truvaluelabs.com>
2021-01-14 19:13:16 -0500

    access origin policy

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const { spawn } = require('child_process')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 app.get('/', (req, res) => {
   let dataToSend
